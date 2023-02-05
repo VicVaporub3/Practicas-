7198b3d828157e4b26afab2e257d1cc4159b1a1a
VicVaporub3 <124392221+VicVaporub3@users.noreply.github.com>
2023-02-04 19:30:29 -0600

    Add files via upload

---
+++ eva1_1_hola_mundo/src/eva1_1_hola_mundo/Eva1_1_hola_mundo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_1_hola_mundo;
+
+/**
+ *
+ * @author victorgrc723
+ */
+public class Eva1_1_hola_mundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//TODO code application logic here 
+int x=100;
+System.out.println("hola mundo!!");
+        System.out.println(x);
+    }
+    
+}
