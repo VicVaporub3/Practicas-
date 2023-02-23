757be85c7136184f897fec9d6635cb89caa2eab1
VicVaporub3 <124392221+VicVaporub3@users.noreply.github.com>
2023-02-22 21:08:05 -0600

    Add files via upload

---
+++ EVA1_10_RETURN/src/eva1_10_return/EVA1_10_RETURN.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_10_return;
+
+/**
+ *
+ * @author victorgrc723
+ */
+public class EVA1_10_RETURN {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        square(10);//llamada a funcion, pero sin procesar el resultado
+        int resu;
+        resu = square(3);//llamada a funcion, almacenando el resultado
+        System.out.println(resu);
+        System.out.println(square(5));//llamada a funcion, enviando el resultado
+    }
+    public static int square(int num){
+        return num * num;
+    }
+    
+}
