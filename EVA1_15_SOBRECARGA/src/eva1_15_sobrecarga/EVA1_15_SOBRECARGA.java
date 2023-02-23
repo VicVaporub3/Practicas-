a5f72de6b7e2311e97a277da6877641bd8f3e629
VicVaporub3 <124392221+VicVaporub3@users.noreply.github.com>
2023-02-23 12:49:29 -0600

    Add files via upload

---
+++ EVA1_15_SOBRECARGA/src/eva1_15_sobrecarga/EVA1_15_SOBRECARGA.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_15_sobrecarga;
+
+/**
+ *
+ * @author victorgrc723
+ */
+public class EVA1_15_SOBRECARGA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("HELLO WORLD");
+        System.out.println(100);
+        System.out.println(true);
+        System.out.println(args);
+    }
+    
+}
