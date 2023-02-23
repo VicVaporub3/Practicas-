43840c801749c96687350abaf326ead20f826bce
VicVaporub3 <124392221+VicVaporub3@users.noreply.github.com>
2023-02-23 12:48:38 -0600

    Add files via upload

---
+++ EVA1_13_ALCANCE_2/src/eva1_13_alcance_2/EVA1_13_ALCANCE_2.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_13_alcance_2;
+
+/**
+ *
+ * @author victorgrc723
+ */
+public class EVA1_13_ALCANCE_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+            System.out.println(v);
+            int v = 100;
+            System.out.println(v);
+            {
+                int m = 1000;
+                System.out.println(m);
+            }
+              System.out.println(m);
+            
+        }
+         System.out.println(i);
+         System.out.println(v);
+    }
+    
+}
