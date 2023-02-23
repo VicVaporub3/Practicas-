f8c6ee188e8d9d9e706a0dd86592eab041729e0c
VicVaporub3 <124392221+VicVaporub3@users.noreply.github.com>
2023-02-23 12:48:05 -0600

    Add files via upload

---
+++ EVA1_12_ALCANCE/src/eva1_12_alcance/EVA1_12_ALCANCE.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_12_alcance;
+
+/**
+ *
+ * @author victorgrc723
+ */
+public class EVA1_12_ALCANCE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println(v);//no existe la v porque esta declarada despues de la instruccion
+        System.out.println(args);//args esta declarado antes de la instruccion
+        int v = 100;
+        System.out.println(v);
+    }
+    
+}
