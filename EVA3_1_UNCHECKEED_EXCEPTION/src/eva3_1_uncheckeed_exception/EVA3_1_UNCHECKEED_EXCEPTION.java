877cd5898885fdf8b0f8fcb504db3cb0045b140f
VicVaporub3 <124392221+VicVaporub3@users.noreply.github.com>
2023-04-26 12:43:47 -0600

    Add files via upload

---
+++ EVA3_1_UNCHECKEED_EXCEPTION/src/eva3_1_uncheckeed_exception/EVA3_1_UNCHECKEED_EXCEPTION.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva3_1_uncheckeed_exception;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA3_1_UNCHECKEED_EXCEPTION {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x = 5;
+        Prueba objPrueba = null;
+        System.out.println("Valor de x = " + objPrueba.x);//No puede leer la x, porque el objeto no existe 
+    }
+    
+}
